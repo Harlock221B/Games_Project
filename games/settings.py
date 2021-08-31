c6252817bae5f4d904eef3962f07263348fb30c3
Gabriel Ferreira <64704484+Harlock221B@users.noreply.github.com>
2021-08-31 10:54:06 -0300

    Correção do erro de Templates Not Found

--- games/settings.py
+++ games/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'games.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'tamplates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
