d5ccf4a2915b4951959f73ac2ede11d38626b8af
Harlock221B <gabrielferreira221b@gmail.com>
2021-09-27 11:36:40 -0300

    User Models

--- home/models.py
+++ home/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Produtora(models.Model):
         name = models.CharField(max_length=60)
         description = models.CharField(max_length=200)
